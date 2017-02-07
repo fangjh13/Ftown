f4ec404e473ac5e5962c4d61293a1feef31f68df
Fython <fang.jia.hui123@gmail.com>
2017-02-07 20:35:55 +0800

    change everyone posts

--- app/blog/views.py
+++ app/blog/views.py
@@ -2,7 +2,7 @@
 
 from . import blog
 from flask import render_template
-from ..models import User
+from ..models import User, Post
 
 @blog.route('/')
 def home():
@@ -18,7 +18,13 @@ def about():
 
 @blog.route('/post')
 def post():
-    return render_template('/blog/post.html')
+    post = Post.query.order_by(Post.timestamp.desc()).first_or_404()
+    return render_template('/blog/post.html', post=post)
+
+@blog.route('/post/<int:post_id>')
+def onepost(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('/blog/post.html', post=post)
 
 
 @blog.route('/contact')
