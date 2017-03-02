91f7ab8c7ec975918082f42c30952fbc9e6387c4
Fython <fang.jia.hui123@gmail.com>
2017-03-02 10:57:59 +0800

    alter app_errorhandler

--- app/blog/__init__.py
+++ app/blog/__init__.py
@@ -5,4 +5,4 @@ from flask import Blueprint
 
 blog = Blueprint('blog', __name__)
 
-from . import views
+from . import views, errors
--- app/blog/errors.py
+++ app/blog/errors.py
@@ -7,9 +7,9 @@ from flask import make_response, jsonify, request, redirect, url_for
 @blog.app_errorhandler(404)
 def not_found(e):
     print(request.accept_mimetypes)
-    if request.accept_mimetypes.accept_json and \
-        not request.accept_mimetypes.accept_html:
+    if 'application/json' in request.accept_mimetypes and \
+        not 'txt/html' in request.accept_mimetypes:
         response = make_response(jsonify({'error': 'not found'}))
         response.status_code = 404
         return response
-    return redirect(url_for('blog.home')), 404
+    return redirect(url_for('blog.home'))
