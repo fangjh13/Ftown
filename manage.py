9bb301ccca3943ae1d4d545f4d93f9d0d3396e61
Fython <fang.jia.hui123@gmail.com>
2017-03-17 10:25:52 +0800

    from `.env` import environment

--- manage.py
+++ manage.py
@@ -4,12 +4,21 @@
 
 import os
 
+# import local environment
+if os.path.exists('.env'):
+    print('Import environment from .env ...')
+    with open('.env') as f:
+        for line in f:
+            key, value = line.strip().split('=')
+            os.environ[key] = value
+
+
 from app import create_app, db
 from app.models import User, Post
 from flask_script import Manager, Shell, Server
 from flask_migrate import Migrate, MigrateCommand
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+app = create_app(os.getenv('FTOWN_CONFIG') or 'default')
 
 manager = Manager(app)
 migrate = Migrate(app, db)
