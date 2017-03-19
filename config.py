7674cbce6954d9bd2dda095ddc13aceaf50cf542
Fython <fang.jia.hui123@gmail.com>
2017-03-19 20:19:02 +0800

    when error send mail

--- config.py
+++ config.py
@@ -51,6 +51,32 @@ class ProductionConfig(Config):
     MAIL_USE_SSL = False
 
 
+    @classmethod
+    def init_app(cls, app):
+        Config.init_app(app)
+
+        # email error to the administrators
+        import logging
+        from logging.handlers import SMTPHandler
+        secure = None
+        credentials = None
+        if getattr(cls, 'MAIL_USERNAME', None):
+            credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
+            if getattr(cls, 'MAIL_USE_TLS', None):
+                secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
+            fromaddr="Blog Admin <{0}>".format(cls.MAIL_USERNAME),
+            toaddrs=['616960344@qq.com'],
+            subject='[Blog] Application Error',
+            credentials=credentials,
+            secure=secure,
+            timeout=10)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+
+
 config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
