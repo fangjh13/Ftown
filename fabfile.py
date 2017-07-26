5caa925ac915574e2ec18bf78a22bc222696fe5b
Fython <fang.jia.hui123@gmail.com>
2017-07-26 16:40:15 +0800

    automatic deployment script

---
+++ fabfile.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
+''' Automatic deployment script '''
+
+
+from fabric.api import env, run, cd, prefix
+import os
+
+if os.path.exists('.env'):
+    print('import env from local')
+    with open('.env') as f:
+        for l in f:
+            key, value = l.strip().split('=')
+            os.environ[key] = value
+
+
+env.host_string = os.getenv('DEPLOY_HOSTS')
+
+
+def deploy():
+    with cd('/srv/Ftown'):
+        with prefix('source ftownvenv/bin/activate'):
+            run('git fetch --all')
+            run('git reset --hard origin/master')
+            run('supervisorctl restart ftown')
